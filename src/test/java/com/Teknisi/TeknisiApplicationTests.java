fc247e36dfcbee464f0ca9b7cbc580cf1b7351fc
Daniel Stephanus Najoan <41951274+danielnajoan@users.noreply.github.com>
2021-05-04 20:08:18 +0700

    Initial commit

---
+++ src/test/java/com/Teknisi/TeknisiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Teknisi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeknisiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
