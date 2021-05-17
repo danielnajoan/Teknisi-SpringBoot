0825a222152ddeb90d6cec26cca0461f33625955
Daniel Stephanus Najoan <41951274+danielnajoan@users.noreply.github.com>
2021-05-17 20:44:06 +0700

    remove unused import

--- src/main/java/com/Teknisi/model/Teknisi.java
+++ src/main/java/com/Teknisi/model/Teknisi.java
@@ -3,9 +3,7 @@ package com.Teknisi.model;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
