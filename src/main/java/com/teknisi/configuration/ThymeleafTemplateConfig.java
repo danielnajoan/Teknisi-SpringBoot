f1aacd07e15eed62a6cddca80f1b875e5f4aaad1
Daniel Stephanus Najoan <41951274+danielnajoan@users.noreply.github.com>
2021-06-15 21:14:47 +0700

    Update Thymeleaf Configuration

--- src/main/java/com/teknisi/configuration/ThymeleafTemplateConfig.java
+++ src/main/java/com/teknisi/configuration/ThymeleafTemplateConfig.java
@@ -3,12 +3,11 @@ package com.teknisi.configuration;
 import java.nio.charset.StandardCharsets;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.templatemode.TemplateMode;
 
-@Configuration
+
 public class ThymeleafTemplateConfig {
     @Bean
     public SpringTemplateEngine springTemplateEngine() {
